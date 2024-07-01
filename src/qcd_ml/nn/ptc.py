a6d31e94c1847b0a05886fd5858432441fb32c1d
Daniel Knuettel <Daniel Knuettel>
2024-07-01 11:15:01 +0200

    added v_PTC

--- src/qcd_ml/nn/ptc.py
+++ src/qcd_ml/nn/ptc.py
@@ -1,6 +1,7 @@
 import torch 
 
-from ..hop import v_hop
+from ..paths import v_evaluate_path
+from ..operations import v_spin_const_transform
 
 class v_PTC(torch.nn.Module):
     """
@@ -12,13 +13,28 @@ class v_PTC(torch.nn.Module):
     paths is a list of paths. Every path is a list [(direction, nhops)].
     An empty list is the path that does not perform any hops.
     """
-    def __init__(self, n_feature_in, n_feature_out, paths):
+    def __init__(self, n_feature_in, n_feature_out, paths, U):
+        super().__init__()
         self.weights = torch.nn.Parameter(
-                torch.complex(torch.tensor(n_feature_in, n_feature_out, len(paths), dtype=torch.double)
-                              , torch.tensor(n_feature_in, n_feature_out, len(paths), dtype=torch.double))
+                torch.complex(torch.randn(n_feature_in, n_feature_out, len(paths), 4, 4, dtype=torch.double)
+                              , torch.randn(n_feature_in, n_feature_out, len(paths), 4, 4, dtype=torch.double))
                 )
 
         self.n_feature_in = n_feature_in
-        self.n_feature_paths = n_feature_paths
+        self.n_feature_out = n_feature_out
+        self.paths = paths
+        self.U = U
 
 
+    def forward(self, features_in):
+        if features_in.shape[0] != self.n_feature_in:
+            raise ValueError(f"shape mismatch: got {features_in.shape[0]} but expected {self.n_feature_in}")
+        
+        features_out = [torch.zeros_like(features_in[0]) for _ in range(self.n_feature_out)]
+
+        for fi, wfi in zip(features_in, self.weights):
+            for io, wfo in enumerate(wfi):
+                for pi, wi in zip(self.paths, wfo):
+                    features_out[io] = features_out[io] + v_spin_const_transform(wi, v_evaluate_path(self.U, pi, fi))
+
+        return torch.stack(features_out)
