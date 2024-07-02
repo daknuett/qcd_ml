5f9f512a165e60a5c3c3ffccfbe330d0e5bdd445
Daniel Knuettel <Daniel Knuettel>
2024-07-02 10:31:27 +0200

    added ptc equivariance test

--- test/test_03_path_evaluate.py
+++ test/test_03_path_evaluate.py
@@ -4,7 +4,7 @@ import numpy as np
 from qcd_ml.hop import v_hop
 from qcd_ml.paths import v_evaluate_path
 
-def test_v_evaluate_path_agains_v_hop(config_1500, psi_test):
+def test_v_evaluate_path_against_v_hop(config_1500, psi_test):
     path = [(0, 1)]
     assert torch.allclose(v_hop(config_1500, 0, 1, psi_test), v_evaluate_path(config_1500, path, psi_test))
 
