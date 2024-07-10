ff929a651a981ba1b04b97d84591e3a627316624
Daniel Knuettel <Daniel Knuettel>
2024-07-10 12:42:47 +0200

    improved some test

--- test/test_05_compat_gpt.py
+++ test/test_05_compat_gpt.py
@@ -13,6 +13,18 @@ try:
 
         rng.cnormal(psi)
 
+        psi2 = ndarray2lattice(lattice2ndarray(psi), grid, g.vspincolor)
+
+        assert g.norm2(psi2 - psi) < 1e-12
+
+    def test_array2lattice2array():
+        grid = g.grid([4, 4, 4, 8], g.double)
+
+        rng = g.random("foo")
+        psi = g.vspincolor(grid)
+
+        rng.cnormal(psi)
+
         ndarray1 = lattice2ndarray(psi)
         ndarray2 = lattice2ndarray(ndarray2lattice(ndarray1, grid, g.vspincolor))
 
