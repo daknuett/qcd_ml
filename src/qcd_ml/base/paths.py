022dc49b182b9fab0501bc1e137b84487b123a41
Daniel Knuettel <Daniel Knuettel>
2024-07-23 14:15:13 +0200

    added base.paths.PathBuffer.v_reverse_transport

--- src/qcd_ml/base/paths.py
+++ src/qcd_ml/base/paths.py
@@ -60,8 +60,32 @@ def v_reverse_evaluate_path(U, path, v):
             v = v_hop(U, mu, direction, v)
     return v
 
+def v_ng_reverse_evaluate_path(path, v):
+    """
+    paths is a list of paths. Every path is a list [(mu, nhops)].
+    An empty list is the path that does not perform any hops.
+    
+    If nhops is negative, the hop is made in negative mu direction.
+    """
+    for mu, nhops in reversed(path):
+        nhops *= -1
+        if nhops < 0:
+            direction = -1
+            nhops *= -1
+        else:
+            direction = 1
+
+        for _ in range(nhops):
+            v = v_ng_hop(mu, direction, v)
+    return v
+
 
 class PathBuffer:
+    """
+    This class brings the same functionality as v_evaluate_path and
+    v_reverse_evaluate_path but pre-computes the costly gauge transport matrix
+    multiplications.
+    """
     def __init__(self, U, path):
         self.path = path
 
@@ -92,3 +116,7 @@ class PathBuffer:
         v = v_ng_evaluate_path(self.path, v)
         return v
 
+    def v_reverse_transport(self, v):
+        v = v_ng_reverse_evaluate_path(self.path, v)
+        v = v_gauge_transform(self.accumulated_U.adjoint(), v)
+        return v
