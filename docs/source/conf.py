eb3a6ed745490a59327f37b3acf85c980ec8ba05
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2024-08-05 13:19:26 +0200

    added more sphinx configuration and tox env to build docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, "../../src/")
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,10 +19,20 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx.ext.autodoc',
+        'sphinx.ext.doctest',
+        'sphinx.ext.coverage',
+        'sphinx.ext.imgmath',
+        'sphinx.ext.ifconfig',
+        'sphinx.ext.viewcode',
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = []
+
+source_suffix = '.rst'
+todo_include_todos = True
 
 
 
