86837def28dd6964678cf2f0109a403f12085fd8
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2024-08-07 13:03:33 +0200

    moved to v0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, "../../src/")
 project = 'qcd_ml'
 copyright = '2024, Daniel Knüttel, et al.'
 author = 'Daniel Knüttel, et al.'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
