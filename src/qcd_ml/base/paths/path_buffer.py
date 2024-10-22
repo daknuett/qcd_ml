5fd197c374a4d00278e5b999938bcd3632220714
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2024-10-22 13:07:43 +0200

    added option to have non-3x3 gauge fields

--- src/qcd_ml/base/paths/path_buffer.py
+++ src/qcd_ml/base/paths/path_buffer.py
@@ -10,11 +10,19 @@ class PathBuffer:
     v_reverse_evaluate_path but pre-computes the costly gauge transport matrix
     multiplications.
     """
-    def __init__(self, U, path):
+    def __init__(self, U, path
+                 , gauge_group_compose=SU3_group_compose
+                 , gauge_transform=v_gauge_transform
+                 , adjoin=lambda x: x.adjoint()
+                 , gauge_identity=torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=torch.cdouble)):
         if isinstance(U, list):
             # required by torch.roll below.
             U = torch.stack(U)
         self.path = path
+
+        self.gauge_group_compose = gauge_group_compose
+        self.gauge_transform = gauge_transform
+        self.adjoin = adjoin
 
         if len(self.path) == 0:
             # save computational cost and memory.
@@ -23,10 +31,7 @@ class PathBuffer:
             self._is_identity = False
 
             self.accumulated_U = torch.zeros_like(U[0])
-            self.accumulated_U[:,:,:,:] = torch.complex(
-                    torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=torch.double)
-                    , torch.zeros(3, 3, dtype=torch.double)
-                    )
+            self.accumulated_U[:,:,:,:] = torch.clone(gauge_identity)
 
             for mu, nhops in self.path:
                 if nhops < 0:
@@ -38,9 +43,9 @@ class PathBuffer:
                 for _ in range(nhops):
                     if direction == -1:
                         U = torch.roll(U, 1, mu + 1) # mu + 1 because U is (mu, x, y, z, t)
-                        self.accumulated_U = SU3_group_compose(U[mu], self.accumulated_U)
+                        self.accumulated_U = self.gauge_group_compose(U[mu], self.accumulated_U)
                     else:
-                        self.accumulated_U = SU3_group_compose(U[mu].adjoint(), self.accumulated_U)
+                        self.accumulated_U = self.gauge_group_compose(self.adjoin(U[mu]), self.accumulated_U)
                         U = torch.roll(U, -1, mu + 1)
 
             self.path = compile_path(self.path)
@@ -50,7 +55,7 @@ class PathBuffer:
         Gauge-equivariantly transport the vector-like field ``v`` along the path.
         """
         if not self._is_identity:
-            v = v_gauge_transform(self.accumulated_U, v)
+            v = self.gauge_transform(self.accumulated_U, v)
             v = v_ng_evaluate_path(self.path, v)
         return v
 
@@ -60,6 +65,6 @@ class PathBuffer:
         """
         if not self._is_identity:
             v = v_ng_reverse_evaluate_path(self.path, v)
-            v = v_gauge_transform(self.accumulated_U.adjoint(), v)
+            v = self.gauge_transform(self.adjoin(self.accumulated_U), v)
         return v
 
