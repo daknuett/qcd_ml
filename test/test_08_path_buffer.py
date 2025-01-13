61967c5e4c00c8dbf57384e5f1755e7e729c214b
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2025-01-13 13:21:00 +0100

    added test for PathBuffer.m_transport equivariance

--- test/test_08_path_buffer.py
+++ test/test_08_path_buffer.py
@@ -6,7 +6,7 @@ import pytest
 from qcd_ml.base.paths import v_evaluate_path, m_evaluate_path
 from qcd_ml.base.paths import v_reverse_evaluate_path
 from qcd_ml.base.paths import PathBuffer
-from qcd_ml.base.operations import v_gauge_transform, link_gauge_transform
+from qcd_ml.base.operations import v_gauge_transform, link_gauge_transform, m_gauge_transform
 
 
 def test_path_buffer_00(config_1500, psi_test):
@@ -63,7 +63,7 @@ def test_path_buffer_reverse(config_1500, psi_test):
     assert torch.allclose(pb.v_reverse_transport(psi), v_reverse_evaluate_path(config_1500, path, psi))
 
 
-def test_path_buffer_equivariance(config_1500, psi_test, V_1500mu0_1500mu2):
+def test_path_buffer_equivariance_v(config_1500, psi_test, V_1500mu0_1500mu2):
     psi_test = torch.randn_like(psi_test)
     V = V_1500mu0_1500mu2
 
@@ -79,3 +79,21 @@ def test_path_buffer_equivariance(config_1500, psi_test, V_1500mu0_1500mu2):
     psibar_gtb = pb_trans.v_transport(psi_test_gt)
     
     assert torch.allclose(psibar_gtb, psibar_gta)
+
+
+def test_path_buffer_equivariance_m(config_1500, V_1500mu0_1500mu2):
+    M_test = torch.randn(8,8,8,16, 3,3, dtype=torch.cdouble)
+    V = V_1500mu0_1500mu2
+
+    path = [(0,1), (2, -2), (3,1)]
+    pb = PathBuffer(config_1500, path)
+    
+    Mbar_ngt = pb.m_transport(M_test)
+    Mbar_gta = m_gauge_transform(V, Mbar_ngt)
+
+    U = link_gauge_transform(config_1500, V)
+    M_test_gt = m_gauge_transform(V, M_test)
+    pb_trans = PathBuffer(U, path)
+    Mbar_gtb = pb_trans.m_transport(M_test_gt)
+    
+    assert torch.allclose(Mbar_gtb, Mbar_gta)
--- test/test_13_matrix_layers.py
+++ test/test_13_matrix_layers.py
@@ -2,7 +2,7 @@ import torch
 
 from qcd_ml.nn.matrix_layers import LGE_Convolution
 from qcd_ml.base.paths import PathBuffer
-from qcd_ml.base.operations import SU3_group_compose, link_gauge_transform
+from qcd_ml.base.operations import m_gauge_transform, link_gauge_transform
 
 
 def test_LGE_Convolution_equivariance(config_1500, V_1500mu0_1500mu2):
@@ -18,11 +18,11 @@ def test_LGE_Convolution_equivariance(config_1500, V_1500mu0_1500mu2):
 
     features_in = torch.randn(1, 8,8,8,16, 3,3, dtype=torch.cdouble)
     features_out = layer.forward(features_in)
-    transformed_after = SU3_group_compose(V_1500mu0_1500mu2, features_out[0])
+    transformed_after = m_gauge_transform(V_1500mu0_1500mu2, features_out[0])
 
     transformed_U = link_gauge_transform(config_1500, V_1500mu0_1500mu2)
     layer.path_buffers = [PathBuffer(transformed_U, path) for path in paths]
 
-    features_out_gt = layer.forward(features_in)
+    features_out_gt = layer.forward(torch.stack([m_gauge_transform(V_1500mu0_1500mu2, features_in[0])]))
 
     assert torch.allclose(transformed_after, features_out_gt[0])
