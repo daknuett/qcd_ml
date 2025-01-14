fa6cdef0cebc89b77f70b576cbd01fb563f2e77e
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2025-01-14 17:15:33 +0100

    dirac operators: copy gamma structures to local device

--- src/qcd_ml/util/__init__.py
+++ src/qcd_ml/util/__init__.py
@@ -1,2 +1,16 @@
+import torch
 import qcd_ml.util.qcd
 import qcd_ml.util.solver
+
+
+def get_device_by_reference(reference: torch.Tensor):
+    """
+    This is a utility function that gives a result that can be passed
+    to ``torch.Tensor.to()``. It takes a reference tensor and allows
+    the user to put a tensor on the same device as the reference tensor.
+    """
+    device = reference.get_device()
+
+    if device == -1:
+        return "cpu"
+    return f"cuda:{device}"
