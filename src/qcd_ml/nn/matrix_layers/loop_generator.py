9cc0708a09b253bbbcb72c2a3605b77ab52b9d6b
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2025-02-04 10:02:05 +0100

    Added *Generator.nfeatures_out that allows for programatic access to number of output features of the generator class

--- src/qcd_ml/nn/matrix_layers/loop_generator.py
+++ src/qcd_ml/nn/matrix_layers/loop_generator.py
@@ -1,19 +1,40 @@
 """
 This module provides loop generator layers, i.e., layers that take a
 link field :math:`U` and compute a set of gauge equivariant loops.
+
+All layers inherit from ``AbstractLoopGenerator`` and provide the class property
+``nfeatures_out``. This allows to access the number of generated loops programatically::
+
+    features_in = PositiveOrientationPlaquetteGenerator.nfeatures_out
+    generator = PositiveOrientationPlaquetteGenerator()
+    ...
 """
 
+from abc import ABCMeta, abstractmethod
 import torch
 from ...base.paths import PathBuffer
 
+class AbstractLoopGenerator(metaclass=ABCMeta):
+    @classmethod
+    @property
+    def nfeatures_out(cls):
+        return cls.property_nfeatures_out
 
-class PolyakovLoopGenerator(torch.nn.Module):
+    @property
+    @abstractmethod
+    def property_nfeatures_out(self):
+        pass
+
+
+class PolyakovLoopGenerator(torch.nn.Module, AbstractLoopGenerator):
     r"""
     Generates the Polyakov loops
     .. math::
         P_\mu(x) = \prod\limits_{k=0}^{L_\mu} U_\mu(x + k\mu)
     for :math:`\mu = 0,1,2,3`.
     """
+
+    property_nfeatures_out = 4  # XXX This expects 4D fields.
 
     def __init__(self, disable_cache=False):
         super(PolyakovLoopGenerator, self).__init__()
@@ -34,12 +55,14 @@ class PolyakovLoopGenerator(torch.nn.Module):
         return loops
 
 
-class PositiveOrientationPlaquetteGenerator(torch.nn.Module):
+class PositiveOrientationPlaquetteGenerator(torch.nn.Module, AbstractLoopGenerator):
     r"""
     Generates the positivly oriented Plaquettes, i.e.,
     .. math::
         P_{\mu\nu}(x) = U_\mu(x) U_\nu(x+\mu) U_\mu^\dagger(x+\nu) U_\nu^\dagger(x)
     """
+
+    property_nfeatures_out = 6
 
     def __init__(self, disable_cache=False):
         super(PositiveOrientationPlaquetteGenerator, self).__init__()
