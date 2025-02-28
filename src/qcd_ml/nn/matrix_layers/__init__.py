0f1b6582c63cc646048c70a7ea29682a6fd4eb76
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2025-02-28 15:16:13 +0100

    added some docs

--- src/qcd_ml/nn/matrix_layers/__init__.py
+++ src/qcd_ml/nn/matrix_layers/__init__.py
@@ -1,7 +1,20 @@
 r"""
+qcd_ml.nn.matrix_layers
+=======================
+
 Layers for matrix valued fields, i.e., fields that transform as
 .. math::
-    M(x) \rightarrow \Omega(x) M(x) \Omega(x)
+    M(x) \rightarrow \Omega(x) M(x) \Omega(x).
+
+Provides the following layers:
+
+- ``LGE_Convolution``
+- ``LGE_Bilinear``
+- ``LGE_ReTrAct``
+- ``LGE_Exp``
+- ``PolyakovLoopGenerator`` and ``PositiveOrientationPlaquetteGenerator``
+
+See [10.1103/PhysRevLett.128.032003].
 """
 
 from .convolution import LGE_Convolution
