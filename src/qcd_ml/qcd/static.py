3a17fd1575a13b0a8db2e754084c3b310b0b389f
Daniel Knuettel <daniel.knuettel@daknuett.eu>
2025-05-28 16:33:18 +0200

    some formatting

--- src/qcd_ml/qcd/static.py
+++ src/qcd_ml/qcd/static.py
@@ -11,22 +11,22 @@ The matrices are chosen to be identical to the default choice
 in `lehner/gpt <https://github.com/lehner/gpt>`_.
 """
 
-gamma = [torch.tensor([[0,0,0,1j]
-                      ,[0,0,1j,0]
-                      ,[0,-1j,0,0]
-                      ,[-1j,0,0,0]], dtype=torch.cdouble)
-         , torch.tensor([[0,0,0,-1]
-                        ,[0,0,1,0]
-                        ,[0,1,0,0]
-                        ,[-1,0,0,0]], dtype=torch.cdouble)
-         , torch.tensor([[0,0,1j,0]
-                        ,[0,0,0,-1j]
-                        ,[-1j,0,0,0]
-                        ,[0,1j,0,0]], dtype=torch.cdouble)
-         , torch.tensor([[0,0,1,0]
-                        ,[0,0,0,1]
-                        ,[1,0,0,0]
-                        ,[0,1,0,0]], dtype=torch.cdouble)
+gamma = [torch.tensor([[0, 0, 0, 1j]
+                      , [0, 0, 1j, 0]
+                      , [0, -1j, 0, 0]
+                      , [-1j, 0, 0, 0]], dtype=torch.cdouble)
+         , torch.tensor([[0, 0, 0, -1]
+                        , [0, 0, 1, 0]
+                        , [0, 1, 0, 0]
+                        , [-1, 0, 0, 0]], dtype=torch.cdouble)
+         , torch.tensor([[0, 0, 1j, 0]
+                        , [0, 0, 0, -1j]
+                        , [-1j, 0, 0, 0]
+                        , [0, 1j, 0, 0]], dtype=torch.cdouble)
+         , torch.tensor([[0, 0, 1, 0]
+                        , [0, 0, 0, 1]
+                        , [1, 0, 0, 0]
+                        , [0, 1, 0, 0]], dtype=torch.cdouble)
          , torch.tensor([[1, 0, 0, 0]
                          , [0, 1, 0, 0]
                          , [0, 0, -1, 0]
