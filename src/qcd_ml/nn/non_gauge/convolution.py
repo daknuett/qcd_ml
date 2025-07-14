4fe7e8bc226b3720a9cff5120cb398a94fb3b06e
gnrraphi <raphael.lehner@gmail.com>
2025-07-14 15:18:14 +0200

    added ValueError for C_Convolution

--- src/qcd_ml/nn/non_gauge/convolution.py
+++ src/qcd_ml/nn/non_gauge/convolution.py
@@ -86,6 +86,10 @@ class C_Convolution(torch.nn.Module):
         """
         nu = U.dim()
 
+        # expected input shape (C, x_0, ..., x_{nd-1}, ...)
+        if nu <= self.nd:
+            raise ValueError(f"shape mismatch: got {nu} but expected bigger than {self.nd}")
+
         # apply padding
         U = self._circular_pad(U)
 
