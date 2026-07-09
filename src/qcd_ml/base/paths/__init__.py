4d6cbcbc0d4df6de92c200d88d2c224f23ca4c68
Simon Pfahler <simon@pfahler.online>
2026-07-09 11:41:40 +0000

    Add type hints and fix typo in base/paths module

--- src/qcd_ml/base/paths/__init__.py
+++ src/qcd_ml/base/paths/__init__.py
@@ -7,10 +7,11 @@ Gauge-equivariant parallel transport paths.
 The function ``v_evaluate_path`` is memory effective but slow.
 
 The class ``PathBuffer`` can be used to speed up path evaluation
-but may be more memory intensve.
-
+but may be more memory intensive.
 
 """
+
+from typing import List, Tuple
 
 from .simple_paths import (
         v_evaluate_path, v_ng_evaluate_path, v_reverse_evaluate_path, v_ng_reverse_evaluate_path, m_evaluate_path
@@ -19,10 +20,15 @@ from .simple_paths import (
 from .path_buffer import PathBuffer
 
 
-def path_get_orig_point(path):
+def path_get_orig_point(path: List[Tuple[int, int]]) -> List[int]:
     """
-    This funciton returns the point that will be transported to the point [0,0,0,0]
-    by the path.
+    Returns the point that will be transported to the point [0,0,0,0] by the path.
+    
+    Args:
+        path: List of path elements, where each element is a tuple (mu, nhops).
+        
+    Returns:
+        The origin point as a list of coordinates.
     """
     point = [0] * 4
     for mu, nhops in path:
