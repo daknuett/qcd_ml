0fccdc12019b1e0ac320c97487405d4207307d55
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2026-07-24 12:25:10 +0200

    removed useless docstring

--- src/qcd_ml/qcd/dirac/coarsened.py
+++ src/qcd_ml/qcd/dirac/coarsened.py
@@ -55,15 +55,6 @@ class coarse_9point_op_NG:
         self.L_coarse = L_coarse
 
         def pseudo_gauge_apply(ps_gauge: torch.Tensor, vec: torch.Tensor) -> torch.Tensor:
-            """Apply pseudo-gauge transformation.
-            
-            Args:
-                ps_gauge: Pseudo-gauge field tensor.
-                vec: Vector to transform.
-                
-            Returns:
-                Transformed vector.
-            """
             return torch.einsum("abcdij,abcdj->abcdi", ps_gauge, vec)
     
         self.pseudo_gauge_transform = pseudo_gauge_apply
